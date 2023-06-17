89657307de1ccc2ff22aac4a198b1843da0338bd
Wong Da You <64577096+wongdayou@users.noreply.github.com>
2023-06-17 22:46:57 +0800

    Changed playermovement script based on new tut

--- Pokemon Turquoise/Assets/Scripts/PlayerMovement.cs
+++ Pokemon Turquoise/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
-    Vector2 movement;
+    Vector2 input;
+    bool isMoving = false;
 
     public Animator animator;
 
     public Transform movePoint;
 
     public LayerMask obstacles;
+    public LayerMask grassLayer;
 
     float horizontalMovement = 0f;
     float verticalMovement = 0f;
@@ -25,83 +27,123 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move the player to the new position
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
 
-        // TODO check if the tile has a chance of encountering a wild pokemon
-        
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
 
-        // only when the player is about to reach the new tile, then we allow checking for movement inputs.
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f){
-
-
-            // get the user input
-            horizontalMovement = Input.GetAxisRaw("Horizontal");
-            verticalMovement = Input.GetAxisRaw("Vertical");
-
-
-            // if the player moves left or right
-            if (Mathf.Abs(horizontalMovement) == 1f){
-
-                // check if there are obstacles. If there are none, allow the player to move to the tile
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontalMovement, 0f, 0f), .2f, obstacles)){
-
-                    // update the position where the player is supposed to move.
-                    movePoint.position += new Vector3(horizontalMovement, 0f, 0f);
-
-
-                    // Update parameters for the animator so that animation will play
-                    animator.SetFloat("Horizontal", horizontalMovement);
-                    animator.SetFloat("Vertical", 0f);
-                    animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
-
-                    if (horizontalMovement == 1){
-                        animator.SetFloat("Direction", 3);
-                    }
-
-                    else if(horizontalMovement == -1){
-                        animator.SetFloat("Direction", 2);
-                    }
-                }
-                
+                if (input.x != 0) input.y = 0;
+                StartCoroutine(Move(targetPos));
             }
-
-            // if the player moves up or down
-            else if (Mathf.Abs(verticalMovement) == 1f){
-
-                // check for obstacles. Allow movement if there are none.
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, verticalMovement, 0f), .2f, obstacles)){
-
-                    // update the position where the player is supposed to move
-                    movePoint.position += new Vector3(0f, verticalMovement, 0f);
-
-                    // update parameters for the animator so that animation will play
-                    animator.SetFloat("Vertical", verticalMovement);
-                    animator.SetFloat("Horizontal", 0f);
-                    animator.SetFloat("Speed", Mathf.Abs(verticalMovement));
-
-                    if(verticalMovement == 1){
-                        animator.SetFloat("Direction", 1);
-                    }
-                    else if(verticalMovement == -1){
-                        animator.SetFloat("Direction", 0);
-                    }
-                }
-
-                
-            }
-
-            // if no input is detected. Set parameters for animator so idle animation is being played. 
-            else{
-                animator.SetFloat("Horizontal", horizontalMovement);
-                animator.SetFloat("Vertical", verticalMovement);
-                animator.SetFloat("Speed", 0f);
-            }
-
         }
 
+
+        // if (Vector3.Distance(transform.position, movePoint.position) > 0){
+        //     // move the player to the new position
+        //     transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        //     // TODO check if the tile has a chance of encountering a wild pokemon
+        //     CheckForEncounters();
+        
+        // }
+        
+
+        // // only when the player is about to reach the new tile, then we allow checking for movement inputs.
+        // if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f){
+
+
+        //     // get the user input
+        //     horizontalMovement = Input.GetAxisRaw("Horizontal");
+        //     verticalMovement = Input.GetAxisRaw("Vertical");
+
+
+        //     // if the player moves left or right
+        //     if (Mathf.Abs(horizontalMovement) == 1f){
+
+        //         // check if there are obstacles. If there are none, allow the player to move to the tile
+        //         if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontalMovement, 0f, 0f), .2f, obstacles)){
+
+        //             // update the position where the player is supposed to move.
+        //             movePoint.position += new Vector3(horizontalMovement, 0f, 0f);
+
+
+        //             // Update parameters for the animator so that animation will play
+        //             animator.SetFloat("Horizontal", horizontalMovement);
+        //             animator.SetFloat("Vertical", 0f);
+        //             animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
+        //             if (horizontalMovement == 1){
+        //                 animator.SetFloat("Direction", 3);
+        //             }
+
+        //             else if(horizontalMovement == -1){
+        //                 animator.SetFloat("Direction", 2);
+        //             }
+        //         }
+                
+        //     }
+
+        //     // if the player moves up or down
+        //     else if (Mathf.Abs(verticalMovement) == 1f){
+
+        //         // check for obstacles. Allow movement if there are none.
+        //         if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, verticalMovement, 0f), .2f, obstacles)){
+
+        //             // update the position where the player is supposed to move
+        //             movePoint.position += new Vector3(0f, verticalMovement, 0f);
+
+        //             // update parameters for the animator so that animation will play
+        //             animator.SetFloat("Vertical", verticalMovement);
+        //             animator.SetFloat("Horizontal", 0f);
+        //             animator.SetFloat("Speed", Mathf.Abs(verticalMovement));
+
+        //             if(verticalMovement == 1){
+        //                 animator.SetFloat("Direction", 1);
+        //             }
+        //             else if(verticalMovement == -1){
+        //                 animator.SetFloat("Direction", 0);
+        //             }
+        //         }
+
+                
+        //     }
+
+        //     // if no input is detected. Set parameters for animator so idle animation is being played. 
+        //     else{
+        //         animator.SetFloat("Horizontal", horizontalMovement);
+        //         animator.SetFloat("Vertical", verticalMovement);
+        //         animator.SetFloat("Speed", 0f);
+        //     }
+
+        // }
+
         
         
+    }
+
+    private void CheckForEncounters(){
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null){
+            if (Random.Range(1,101) <= 10){
+                Debug.Log("Pokemon encountered");
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon){
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
     }
 
 

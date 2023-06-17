57a0d5834ca35222ff54bf0160af341e9d235154
Wong Da You <64577096+wongdayou@users.noreply.github.com>
2023-06-17 14:22:06 +0800

    Adjusted movement and started on spawning feature

--- Pokemon Turquoise/Assets/Scripts/PlayerMovement.cs
+++ Pokemon Turquoise/Assets/Scripts/PlayerMovement.cs
@@ -25,17 +25,32 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // move the player to the new position
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
+        // TODO check if the tile has a chance of encountering a wild pokemon
+        
+
+        // only when the player is about to reach the new tile, then we allow checking for movement inputs.
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f){
 
+
+            // get the user input
             horizontalMovement = Input.GetAxisRaw("Horizontal");
             verticalMovement = Input.GetAxisRaw("Vertical");
 
+
+            // if the player moves left or right
             if (Mathf.Abs(horizontalMovement) == 1f){
+
+                // check if there are obstacles. If there are none, allow the player to move to the tile
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(horizontalMovement, 0f, 0f), .2f, obstacles)){
+
+                    // update the position where the player is supposed to move.
                     movePoint.position += new Vector3(horizontalMovement, 0f, 0f);
 
+
+                    // Update parameters for the animator so that animation will play
                     animator.SetFloat("Horizontal", horizontalMovement);
                     animator.SetFloat("Vertical", 0f);
                     animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
@@ -50,10 +65,17 @@ public class PlayerMovement : MonoBehaviour
                 }
                 
             }
+
+            // if the player moves up or down
             else if (Mathf.Abs(verticalMovement) == 1f){
+
+                // check for obstacles. Allow movement if there are none.
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, verticalMovement, 0f), .2f, obstacles)){
+
+                    // update the position where the player is supposed to move
                     movePoint.position += new Vector3(0f, verticalMovement, 0f);
 
+                    // update parameters for the animator so that animation will play
                     animator.SetFloat("Vertical", verticalMovement);
                     animator.SetFloat("Horizontal", 0f);
                     animator.SetFloat("Speed", Mathf.Abs(verticalMovement));
@@ -68,6 +90,8 @@ public class PlayerMovement : MonoBehaviour
 
                 
             }
+
+            // if no input is detected. Set parameters for animator so idle animation is being played. 
             else{
                 animator.SetFloat("Horizontal", horizontalMovement);
                 animator.SetFloat("Vertical", verticalMovement);
